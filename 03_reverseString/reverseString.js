7de02eb5800ad3099cb68cd8f2a830c925142e2e
Ahmed A <ahmed.elmohanned@gmail.com>
2024-02-10 05:04:46 +0800

    completed js excersie03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) 
+{   
+    const charArray = string.split("");
+    let result = "";
 
+    while(charArray.length != 0)
+    {
+        result += charArray.pop();
+    }
+    return result;
 };
 
 // Do not edit below this line
