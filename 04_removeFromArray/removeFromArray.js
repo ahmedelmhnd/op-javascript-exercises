1ddeee2e2c68d8ed740ff152a9fd216b6da4e494
Ahmed A <ahmed.elmohanned@gmail.com>
2024-02-10 20:18:19 +0800

    completed js excersise04 on arrays and unit testing

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(pArray, ...options ) 
+{
+    for (let i = 0; i < options.length; i++) 
+    {
+        while (pArray.includes(options[i]) == true) 
+        {
+            let idx = pArray.indexOf(options[i]);
+            pArray.splice(idx, 1)
+            
+        }
+        
+    }
 
+    return pArray;
 };
 
 // Do not edit below this line
