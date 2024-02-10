68cd22dbae581eb8e7fc882aeba2ef30dda47519
Ahmed A <ahmed.elmohanned@gmail.com>
2024-02-10 20:39:14 +0800

    comleted js excersise05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) 
+{
+    let result;
+    if (typeof(num1) == "number" & typeof(num2) == "number" & num1 >= 0 & num2 >= 0) 
+    {
+        let temp;
+        result = 0;
+    
+    if (num1 > num2) 
+    {
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
 
+    for (let i = 0; i <= num2 - num1 ; i++) 
+    {
+        result += num1 + i;   
+    }
+        
+    }else
+    {
+        result = "ERROR"
+    }
+    
+
+    return result;
 };
 
 // Do not edit below this line
